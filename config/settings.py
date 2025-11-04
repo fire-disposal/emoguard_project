c55bd7723059a53480c1eaecb01bf69c31e2ec04
FireDisposal <3295829485@qq.com>
2025-11-05 02:46:11 +0800

    CSRF豁免

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,14 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:9000",
     "http://127.0.0.1:9000",
+    "https://cg.aoxintech.com",
+    "http://cg.aoxintech.com",
+]
+
+# CSRF配置
+CSRF_TRUSTED_ORIGINS = [
+    "https://cg.aoxintech.com",
+    "http://cg.aoxintech.com",
 ]
 
 # 缓存配置
